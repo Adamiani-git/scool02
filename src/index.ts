e631eb36ea2113d0127d25b0dd3d3a6239bcd9da
Adamiani-git <adamianismail@gmail.com>
2022-04-12 18:09:32 +0400

    axali versia

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,22 @@
-
 import "./style.css";
 
-// This example requires the Places library. Include the libraries=places
-// parameter when you first load the API. For example:
-// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+
+
+
 
 function initMap(): void {
+
+  // navigator.geolocation.getCurrentPosition(function (position) {
+  //   const lat = position.coords.latitude
+  //   const long = position.coords.longitude
+  // })
+  console.log(navigator.geolocation.getCurrentPosition(function (p) {
+    console.log(p)
+    
+  }));
+  
   // Create the map.
-  const pyrmont = { lat: 41.7195755, lng: 44.7067874};
+  const pyrmont = { lat: 41, lng: 44.7067874 };
   const map = new google.maps.Map(
     document.getElementById("map") as HTMLElement,
     {
@@ -16,10 +25,6 @@ function initMap(): void {
       mapId: "8d193001f940fde3",
     } as google.maps.MapOptions
   );
-
- 
-  
-  
 
   // Create the places service.
   const service = new google.maps.places.PlacesService(map);
@@ -73,31 +78,129 @@ function addPlaces(
         scaledSize: new google.maps.Size(25, 25),
       };
 
-     const marker = new google.maps.Marker({
-        map,
-        icon: image,
-        title: place.name,
-        position: place.geometry.location,
-        animation: google.maps.Animation.DROP,
-      });
-      
-            const contentString =
-            `${place.name}`
+    
+
+      const contentString = `${place.name}`;
 
       const infowindow = new google.maps.InfoWindow({
         content: contentString,
-        maxWidth:250
+        maxWidth: 250,
       });
 
-      marker.addListener("click", () => {
-        infowindow.close()
-        infowindow.open({
-          anchor: marker,
-          map,
-          shouldFocus: true,
-        });
+      // console.log(place);
+      console.log(place);
+
+      const service = new google.maps.places.PlacesService(map);
+
+      const marker = new google.maps.Marker({
+        map,
+        // icon: image,
+        // title: place.name,
+        position: place.geometry.location,
+        animation: google.maps.Animation.DROP,
       });
-    
+
+      // marker.addListener("click", () => {
+      //   infowindow.close();
+      //   infowindow.open({
+      //     anchor: marker,
+      //     map,
+      //     shouldFocus: true,
+      //   });
+      // });
+
+      marker.addListener("mouseover", function () {
+        const request = {
+          placeId: `${place.place_id}`,
+          fields: [
+            "name",
+            "formatted_address",
+            "place_id",
+            "geometry",
+            "photo",
+            "rating",
+            "user_ratings_total",
+          ],
+        };
+
+        service.getDetails(request, (place, status) => {
+          if (
+            status === google.maps.places.PlacesServiceStatus.OK &&
+            place &&
+            place.geometry &&
+            place.geometry.location
+          ) {
+            // const marker = new google.maps.Marker({
+            //   map,
+            //   position: place.geometry.location,
+            // });
+            
+            infowindow.open({
+              map, 
+              anchor:marker,
+              shouldFocus: true,
+              });
+
+            let imgCont='';
+
+            if (place.photos && place.photos.length > 0) {
+              imgCont =
+                "<img src='" +
+                `${place.photos[0].getUrl({
+                  maxWidth: 200,
+                  maxHeight: 150,
+                })}` +
+                "'></img>";
+            }
+            infowindow.setContent(
+              "<div class='infowindow-container'>" +
+                imgCont +
+                "<div class='inner'><h4>" +
+                place.name +
+                "</h4><h5>" +
+                place.formatted_address +
+                "</h5>" +
+                place.international_phone_number +
+                "<p>Rating: " +
+                place.rating +
+                "</p><p>Total reviews: " +
+                place.user_ratings_total +
+                "</p></div></div>"
+            );
+          }
+        });
+      }); //end mouse over
+
+      // infowindow.addListener("cli",function () {
+      //   infowindow.close();
+      // })
+
+      // marker.addListener("mouseout", function () {
+        
+      // });
+
+      // google.maps.event.addListener(marker, "click", () => {
+
+      //   const content = document.createElement("div");
+
+      //   const nameElement = document.createElement("h2");
+
+      //   nameElement.textContent = place.name!;
+      //   content.appendChild(nameElement);
+
+      //   const placeIdElement = document.createElement("p");
+
+      //   placeIdElement.textContent = place.place_id!;
+      //   content.appendChild(placeIdElement);
+
+      //   const placeAddressElement = document.createElement("p");
+
+      //   placeAddressElement.textContent = place.formatted_address!;
+      //   content.appendChild(placeAddressElement);
+
+      //   infowindow.setContent(content);
+      //   infowindow.open(map, marker);
+      // });
 
       const li = document.createElement("li");
 
